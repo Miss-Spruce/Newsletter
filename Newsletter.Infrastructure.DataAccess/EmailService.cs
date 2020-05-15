8b4dd68055d61ef34a3e7e6a73aaf3e36ab7aa9d
Miss-Spruce <Lena@getacademy.no>
2020-05-15 14:06:09 +0200

    fix

---
+++ Newsletter.Infrastructure.DataAccess/EmailService.cs
@@ -0,0 +1,17 @@
+﻿using Newsletter.Core.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newsletter.Core.Domain.Service;
+using System.Threading.Tasks;
+
+namespace Newsletter.Core.Domain.Service
+{
+     class EmailService : IEmailService
+    {
+        public Task<bool> Send(Email email)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
